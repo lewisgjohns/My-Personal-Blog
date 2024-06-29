af4838f419cccfee9e8dbd577822742d91a7ddbb
lewisjohns <lewisjohns@live.com.au>
2024-06-29 11:02:33 +1000

    changes made to javascript to fix toggle button and blog posts saving in local storage

--- Assets/JS/blog.js
+++ Assets/JS/blog.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const blogList = document.getElementById('blogList');
   
     // Check localStorage for existing blog posts
-    const posts = JSON.parse(localStorage.getItem('blogPosts')) || [];
+    const posts = JSON.parse(localStorage.getItem('blogPost')) || [];
   
     // Function to render blog posts
     function renderBlogPosts() {
@@ -20,11 +20,20 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     }
   
+    
     // Initial rendering of blog posts
     renderBlogPosts();
   
     // Event listener for theme toggle button
     themeToggleBtn.addEventListener('click', function() {
-      document.body.classList.toggle('dark-theme');
+       document.body.classList.toggle('dark-theme');
+      console.log(document.body.style.backgroundColor)
+      // if(document.body.style.backgroundColor === "rgb(51, 51, 51)"){
+      //   console.log("it is in dark mode")
+      // }else{
+      //   console.log("not workingn")
+      // }
+      // document.body.style.backgroundColor = "#333333"
+
     });
   });
--- Assets/JS/form.js
+++ Assets/JS/form.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const blogForm = document.getElementById('blogForm');
+
+       // Check localStorage for existing blog posts
+    const posts = JSON.parse(localStorage.getItem('blogPost')) || [];
   
     blogForm.addEventListener('submit', function(event) {
       event.preventDefault();
@@ -14,14 +17,16 @@ document.addEventListener('DOMContentLoaded', function() {
         alert('Please complete all fields.');
         return;
       }
-  
+      
       // Store data in localStorage
       const postData = {
         username,
         title,
         content
       };
-      localStorage.setItem('blogPost', JSON.stringify(postData));
+
+      posts.push(postData)
+      localStorage.setItem('blogPost', JSON.stringify(posts));
   
       // Redirect to posts page (for demonstration, redirecting to a new page)
       window.location.href = 'blog.html';
